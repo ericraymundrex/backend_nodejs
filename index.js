3285acb21c964930b5fcb5552e5ff97aaf6fd2ef
Eric Raymund Rex Sylvester <ericrex09@gmail.com>
2022-03-08 21:49:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+
+const app=express()
+
+const PORT=3000 || process.env.PORT
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Hello");
+});
+app.get("/api/instagram/v1/:name",(req,res)=>{
+    inst_res={
+        name:"Eric",
+        followers: 2000,
+        following: 4000,
+        name:req.params.name
+    };
+
+    res.status(200).json(inst_res);
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running at : ${PORT}\nReach http://localhost:${PORT}/`);
+});
